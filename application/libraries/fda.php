eaf8e3f6dc5360835defe0d890b0c986075bd06c
pimkorschner <pim@pimkorschner.nl>
2014-08-06 17:42:31 +0200

    Added option for brand names

--- application/libraries/fda.php
+++ application/libraries/fda.php
@@ -13,10 +13,7 @@ class Fda {
 
 	function sideEffectId($sideEffectName) {
 		$result = mysql_query("SELECT * FROM side_effects WHERE name = '$sideEffectName'");
-// 		echo 'sideeffectId';
-// 		var_dump($result);
 			if (!$result || mysql_num_rows($result) == 0) {
-// 				$test = mysql_query("INSERT INTO side_effects VALUES ('', '$sideEffectName', '')");
 				$arr = array('name' => $sideEffectName);
 				$test = $this->CI->side_effects_model->insert($arr);
 				return mysql_insert_id();
@@ -28,36 +25,24 @@ class Fda {
 	}
 
 	function sideEffectLookupId($drugId, $sideEffectId) {
-// 		echo "Inserting new side effect lookup: <br/>";
-// 		var_dump($drugId, $sideEffectId);
 		$result = mysql_query("SELECT * FROM side_effects_lookup WHERE drug_id = $drugId AND effect_id = $sideEffectId");
-// 		echo "SELECT * FROM side_effects_lookup WHERE drug_id = $drugId AND effect_id = $sideEffectId <br/>";
-// 		var_dump($result);
 			if (!$result || mysql_num_rows($result) == 0) {
-// 				echo "Adding new side effect as none found <br/>";
-// 				mysql_query("INSERT INTO side_effects_lookup VALUES ('', $drugId, $sideEffectId, '', '', '')");
 				$arr = array('drug_id' => $drugId, 'effect_id' => $sideEffectId);
 				$test = $this->CI->side_effects_lookup_model->insert($arr);
 				return mysql_insert_id();
 			}
 			else {
 				$row = mysql_fetch_array($result);
-// 				echo "Using existing side effect " . $row['id'] . " name: " . $row['drug_id'];
 				return $row['id'];
 			}
 	}
 
 	function drugId($drugName) {
 		$result = mysql_query("SELECT * FROM drugs WHERE generic_name = '$drugName'");
-// 		echo "Drug ID: SELECT * FROM drugs WHERE 'generic_name' = '$drugName' <br/>";
 
 			if (!$result || mysql_num_rows($result) == 0) {
-// 				$test = mysql_query("INSERT INTO drugs VALUES ('', '$drugName', '')");
-// 				$query = "INSERT INTO drugs VALUES ('', '$drugName', '')";
-// 				$test = $this->db->query($query);
 				$arr = array('generic_name' => $drugName);
 				$test = $this->CI->drugs_model->insert($arr);
-// 				echo 'inserting drug:';
 				return mysql_insert_id();
 			}
 			else {
@@ -81,29 +66,32 @@ class Fda {
 
 	function getDrug($drugGenericName) {
 		
-// 		var_dump($this->uri->segment_array());
-		
-// 		$drugGenericName = end($this->uri->segment_array());
-		
 		$client = new GuzzleHttp\Client();
 
-		//$urlAll = "https://api.fda.gov/drug/event.json?search=patient.drug.openfda.generic_name:" . $drugGenericName . "&count=patient.reaction.reactionmeddrapt.exact";
 		$urlMale = "https://api.fda.gov/drug/event.json?search=patient.drug.openfda.generic_name:" . $drugGenericName . "+AND+patient.patientsex:1&count=patient.reaction.reactionmeddrapt.exact&api_key=HhASVaQrzWcEEDhpEZdfOPiBtVggxepGbDviSuIg";
 		$urlFemale = "https://api.fda.gov/drug/event.json?search=patient.drug.openfda.generic_name:" . $drugGenericName . "+AND+patient.patientsex:2&count=patient.reaction.reactionmeddrapt.exact&api_key=HhASVaQrzWcEEDhpEZdfOPiBtVggxepGbDviSuIg";
-
-		$res = $client->get($urlMale, []);
-		$array = $res->json();
-
-        if ($array['error']) {
-            $error = $array['error'];
-            if ($error['code'] == "NOT FOUND") {
-                $urlMale = "https://api.fda.gov/drug/event.json?search=patient.drug.openfda.brand_name:" . $drugGenericName . "+AND+patient.patientsex:1&count=patient.reaction.reactionmeddrapt.exact&api_key=HhASVaQrzWcEEDhpEZdfOPiBtVggxepGbDviSuIg";
-                $urlFemale = "https://api.fda.gov/drug/event.json?search=patient.drug.openfda.brand_name:" . $drugGenericName . "+AND+patient.patientsex:2&count=patient.reaction.reactionmeddrapt.exact&api_key=HhASVaQrzWcEEDhpEZdfOPiBtVggxepGbDviSuIg";
-
+		try {
+			$res = $client->get($urlMale, []);
+			exit;
+			$array = $res->json();
+	
+	        if ($array['error']) {
+	            $error = $array['error'];
+	            if ($error['code'] == "NOT FOUND") {
+	                
+	            }
+	        }
+		} catch (Exception $e) {
+			try {
+			$urlMale = "https://api.fda.gov/drug/event.json?search=patient.drug.openfda.brand_name:" . $drugGenericName . "+AND+patient.patientsex:1&count=patient.reaction.reactionmeddrapt.exact&api_key=HhASVaQrzWcEEDhpEZdfOPiBtVggxepGbDviSuIg";
+	        $urlFemale = "https://api.fda.gov/drug/event.json?search=patient.drug.openfda.brand_name:" . $drugGenericName . "+AND+patient.patientsex:2&count=patient.reaction.reactionmeddrapt.exact&api_key=HhASVaQrzWcEEDhpEZdfOPiBtVggxepGbDviSuIg";
+	
                 $res = $client->get($urlMale, []);
                 $array = $res->json();
-            }
-        }
+			} catch (Exception $e) {
+				exit;
+			}
+		}
 
 		$resultsMale = $array['results'];
 
@@ -120,7 +108,6 @@ class Fda {
 			$sideEffectLookupId = $this->sideEffectLookupId($drugId, $sideEffectId);
 
 			mysql_query("UPDATE side_effects_lookup SET fda_count_male = $eventCount WHERE id = $sideEffectLookupId LIMIT 1");
-// 			echo "SQL QUERY: UPDATE side_effects_lookup SET fda_count_male = $eventCount WHERE id = $sideEffectLookupId LIMIT 1 <br/>";
 		}
 
 		for ($i = 0; $i < count($resultsFemale); $i++) {
@@ -130,7 +117,6 @@ class Fda {
 			$sideEffectLookupId = $this->sideEffectLookupId($drugId, $sideEffectId);
 
 			mysql_query("UPDATE side_effects_lookup SET fda_count_female = $eventCount WHERE id = $sideEffectLookupId LIMIT 1");
-// 			echo "SQL QUERY: UPDATE side_effects_lookup SET fda_count_female = $eventCount WHERE id = $sideEffectLookupId LIMIT 1 <br/>";
 		}
 	}
 }
